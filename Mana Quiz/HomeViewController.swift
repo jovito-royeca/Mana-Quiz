02cf9dfdde6980230bb28dc351c83d9959e97f46
Jovito Royeca <jovit.royeca@gmail.com>
2017-05-21 21:37:16 +0800

    feat: Temporary Home Screen

---
+++ Mana Quiz/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewController.swift
+//  Mana Quiz
+//
+//  Created by Jovito Royeca on 21/05/2017.
+//  Copyright © 2017 Jovito Royeca. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+//        view.backgroundColor = UIColor(patternImage: UIImage(contentsOfFile: "\(Bundle.main.bundlePath)/images/Gray_Patterned_BG.jpg")!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
