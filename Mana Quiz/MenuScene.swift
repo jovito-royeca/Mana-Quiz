20272988e0a6ed146326d076a8c8da774862d5e6
Jovito Royeca <jovit.royeca@gmail.com>
2017-05-24 15:25:25 +0800

    feat: Added mana images

---
+++ Mana Quiz/MenuScene.swift
@@ -0,0 +1,13 @@
+//
+//  MenuScene.swift
+//  Mana Quiz
+//
+//  Created by Jovito Royeca on 24/05/2017.
+//  Copyright © 2017 Jovito Royeca. All rights reserved.
+//
+
+import UIKit
+
+class MenuScene: SKScene {
+
+}
